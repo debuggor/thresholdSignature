dcf69e9bcf407ee8a4dd199bab7235d0d414200b
yong.huang <yong.huang@okcoin.com>
2020-05-31 22:01:59 +0800

    sign first bad

--- src/main/java/com/debuggor/ecdsa/signing/Signing.java
+++ src/main/java/com/debuggor/ecdsa/signing/Signing.java
@@ -3,9 +3,15 @@ package com.debuggor.ecdsa.signing;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.debuggor.common.ReadWriteJson;
+import com.debuggor.crypto.paillier.PrivateKey;
+import com.debuggor.crypto.paillier.PublicKey;
+import com.debuggor.crypto.share.ShareProtocol;
 import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.Sha256Hash;
+import org.spongycastle.math.ec.ECPoint;
 
 import java.math.BigInteger;
+import java.security.SecureRandom;
 
 /**
  * @Author:yong.huang
@@ -18,17 +24,17 @@ public class Signing {
      */
     public static void signing(int threshold, byte[] message) {
         BigInteger q = ECKey.CURVE.getN();
-     //   BigInteger m = calculateE(q, message);
+        BigInteger m = calculateE(q, message);
 
         JSONArray jsonArray = readParams(threshold);
         BigInteger[] Xi = new BigInteger[threshold + 1];
         BigInteger[] ks = new BigInteger[threshold + 1];
-        // BigInteger[] Xi = new BigInteger[threshold+1];
+        JSONObject[] PaillierSKs = new JSONObject[threshold + 1];
         for (int i = 0; i < threshold + 1; i++) {
             JSONObject param = (JSONObject) jsonArray.get(i);
             Xi[i] = param.getBigInteger("Xi");
             ks[i] = param.getBigInteger("ShareID");
-
+            PaillierSKs[i] = param.getJSONObject("PaillierSK");
         }
 
         BigInteger tmp = BigInteger.ZERO;
@@ -36,13 +42,129 @@ public class Signing {
         for (int i = 0; i < threshold + 1; i++) {
             Wi[i] = calculateWi(i, Xi[i], ks);
             tmp = tmp.add(Wi[i]);
-            System.out.println(Wi[i].toString(16));
         }
-        System.out.println("===========================");
-        System.out.println(tmp.toString(16));
-        ECKey ecKey = ECKey.fromPrivate(tmp.mod(q));
-        System.out.println(ecKey.getPrivateKeyAsHex());
-        System.out.println(ecKey.getPublicKeyAsHex());
+
+        // 每个参与者生成两个随机数 k和gamma
+        BigInteger[] Ki = new BigInteger[threshold + 1];
+        BigInteger[] Gamma = new BigInteger[threshold + 1];
+        ECPoint[] PointGamma = new ECPoint[threshold + 1];
+        for (int i = 0; i < threshold + 1; i++) {
+            BigInteger ki = BigInteger.probablePrime(q.bitLength(), new SecureRandom());
+            BigInteger gammai = BigInteger.probablePrime(q.bitLength(), new SecureRandom());
+            Ki[i] = ki;
+            Gamma[i] = gammai;
+            PointGamma[i] = ECKey.publicPointFromPrivate(gammai);
+        }
+
+        //=========== alice和bob交换数据 ===============
+        // step1 alice将自己的数据加密发送给bob
+        BigInteger[] Ca = new BigInteger[threshold + 1];
+        for (int i = 0; i < threshold + 1; i++) {
+            JSONObject alice = PaillierSKs[i];
+            BigInteger N = alice.getBigInteger("N");
+            PublicKey publicKeyA = new PublicKey(N);
+            BigInteger k = Ki[i];
+            BigInteger ca = ShareProtocol.aliceInit(publicKeyA, k);
+            Ca[i] = ca;
+        }
+
+        // step2 bob将alice发送给的数据，加上自己的数据加密后给alice
+        BigInteger[][] Betas = new BigInteger[threshold + 1][threshold + 1];
+        BigInteger[][] V1 = new BigInteger[threshold + 1][threshold + 1];
+        BigInteger[][] Cbwi = new BigInteger[threshold + 1][threshold + 1];
+        BigInteger[][] Cbgamma = new BigInteger[threshold + 1][threshold + 1];
+        for (int i = 0; i < threshold + 1; i++) {
+            BigInteger gammaBob = Gamma[i];
+            BigInteger wiBob = Wi[i];
+            for (int j = 0; j < threshold + 1; j++) {
+                if (i == j) {
+                    continue;
+                }
+                JSONObject alice = PaillierSKs[j];
+                BigInteger N = alice.getBigInteger("N");
+                PublicKey publicKeyA = new PublicKey(N);
+                BigInteger ca = Ca[j];
+
+                BigInteger v1 = BigInteger.ZERO.subtract(BigInteger.probablePrime(q.bitLength(), new SecureRandom()));
+                BigInteger cbwi = ShareProtocol.bobAction(publicKeyA, ca, wiBob, v1);
+                V1[i][j] = v1;
+                Cbwi[i][j] = cbwi;
+
+                BigInteger beta = BigInteger.ZERO.subtract(BigInteger.probablePrime(q.bitLength(), new SecureRandom()));
+                BigInteger cbgamma = ShareProtocol.bobAction(publicKeyA, ca, gammaBob, beta);
+                Betas[i][j] = beta;
+                Cbgamma[i][j] = cbgamma;
+            }
+        }
+
+        // step3 alice加密bob加密的信息 得到自己需要的信息
+        BigInteger[][] V2 = new BigInteger[threshold + 1][threshold + 1];
+        BigInteger[][] Alphas = new BigInteger[threshold + 1][threshold + 1];
+        for (int i = 0; i < threshold + 1; i++) {
+            JSONObject alice = PaillierSKs[i];
+            BigInteger phiN = alice.getBigInteger("PhiN");
+            BigInteger lambdaN = alice.getBigInteger("LambdaN");
+            BigInteger N = alice.getBigInteger("N");
+            PublicKey publicKey = new PublicKey(N);
+            PrivateKey privateKeyA = new PrivateKey(publicKey, lambdaN, phiN);
+
+            for (int j = 0; j < threshold + 1; j++) {
+                if (i == j) {
+                    continue;
+                }
+                BigInteger v2 = ShareProtocol.aliceEnd(privateKeyA, Cbwi[j][i]);
+                BigInteger alphaIj = ShareProtocol.aliceEnd(privateKeyA, Cbgamma[j][i]);
+                V2[i][j] = v2;
+                Alphas[i][j] = alphaIj;
+            }
+        }
+
+        // 每个参与者从其他参与者那获取数据后 计算自己的
+        BigInteger[] Theltas = new BigInteger[threshold + 1];
+        BigInteger[] Sigmas = new BigInteger[threshold + 1];
+        for (int i = 0; i < threshold + 1; i++) {
+            Theltas[i] = Ki[i].multiply(Gamma[i]).mod(q);
+            Sigmas[i] = Ki[i].multiply(Wi[i]).mod(q);
+            for (int j = 0; j < threshold + 1; j++) {
+                if (i == j) {
+                    continue;
+                }
+                Theltas[i] = Theltas[i].add(Alphas[i][j].add(Betas[j][i])).mod(q);
+                Sigmas[i] = Sigmas[i].add(V2[i][j].add(V1[j][i])).mod(q);
+            }
+        }
+
+        // theta的负1次方
+        BigInteger thetaInverse = BigInteger.ZERO;
+        for (int i = 0; i < threshold + 1; i++) {
+            thetaInverse = thetaInverse.add(Theltas[i]).mod(q);
+        }
+        thetaInverse = thetaInverse.modInverse(q);
+
+        // 求得签名中的r
+        ECPoint R = PointGamma[0];
+        for (int i = 1; i < threshold + 1; i++) {
+            R = R.add(PointGamma[i]);
+        }
+        R = R.multiply(thetaInverse);
+        BigInteger rx = R.getXCoord().toBigInteger().mod(q);
+
+        // 每个参与者签名
+        BigInteger[] Signaturei = new BigInteger[threshold + 1];
+        for (int i = 0; i < threshold + 1; i++) {
+            BigInteger ki = Ki[i];
+            BigInteger sigma = Sigmas[i];
+            BigInteger si = ki.multiply(m).add(rx.multiply(sigma)).mod(q);
+            Signaturei[i] = si;
+        }
+
+        // 签名已经完成， 把每个签名者的签名结果相加
+        BigInteger s = BigInteger.ZERO;
+        for (int i = 0; i < threshold + 1; i++) {
+            s = s.add(Signaturei[i]).mod(q);
+        }
+        System.out.println("签名结果s: " + s.toString(16));
+        System.out.println("签名结果r: " + rx.toString(16));
 
     }
 
@@ -95,9 +217,25 @@ public class Signing {
     }
 
     public static void main(String[] args) {
-        signing(2, null);
+        Sha256Hash sha256Hash = Sha256Hash.twiceOf("Hello world!".getBytes());
+        signing(2, sha256Hash.getBytes());
 
-        ECKey ecKey = new ECKey();
-        //  ecKey.sign()
+        /**
+         * 私钥：a449b5df71dd21b07370a848b66b85137b31b46c43645dd8ad3a5b48b4a3fac0
+         * 公钥：02b7844b6be8d0c0d86a1c0dbcaa7dec428ba9ae83aca993b3f37c13f02a80e128
+         *
+         * 签名结果s: ee2582c798888dde0a48eb730669121f2c5d4355cf7ded288cf3d209f37d0cac
+         * 签名结果r: 5b9714e0e3c03bee48c5b238551b95934fd6a781bb9bc27e783e6fb5ca646595
+         */
+        BigInteger pri = new BigInteger("a449b5df71dd21b07370a848b66b85137b31b46c43645dd8ad3a5b48b4a3fac0", 16);
+        ECKey ecKey = ECKey.fromPrivate(pri);
+        System.out.println(ecKey.getPublicKeyAsHex());
+        System.out.println(ecKey.getPrivateKeyAsHex());
+
+        BigInteger r = new BigInteger("ee2582c798888dde0a48eb730669121f2c5d4355cf7ded288cf3d209f37d0cac", 16);
+        BigInteger s = new BigInteger("5b9714e0e3c03bee48c5b238551b95934fd6a781bb9bc27e783e6fb5ca646595", 16);
+        ECKey.ECDSASignature sign = new ECKey.ECDSASignature(r, s);
+        boolean verify = ecKey.verify(sha256Hash, sign);
+        System.out.println(verify);
     }
 }
